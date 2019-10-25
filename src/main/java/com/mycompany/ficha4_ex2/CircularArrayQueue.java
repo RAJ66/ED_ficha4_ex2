135572f502023a326dc4859c91ee1864feb51bfb
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-10-25 14:33:37 +0100

    CircularArrayQueue start

---
+++ src/main/java/com/mycompany/ficha4_ex2/CircularArrayQueue.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ficha4_ex2;
+
+/**
+ *
+ * @author vitor
+ */
+public class CircularArrayQueue<T> implements QueueADT<T>{
+
+    @Override
+    public void enqueue(T element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T dequeue() throws EmptyCollectionException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public T first() throws EmptyCollectionException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
